4eb8bd4c93de9ef6eab704d1c174705caf1d4c96
Deni Maulana Shobri <deni.shobri@muf.co.id>
2026-09-21 10:54:48 +0700

    Fix: limit paralel process while deploying

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: { 
+    cpus: 1,
+  },
   images: {
     remotePatterns: [
       {
@@ -11,8 +14,7 @@ const nextConfig = {
         protocol: "https",
         hostname: "plus.unsplash.com",
       },
-      {
-        // Supabase Storage public objects: <project-ref>.supabase.co
+      { 
         protocol: "https",
         hostname: "*.supabase.co",
         pathname: "/storage/v1/object/public/**",
